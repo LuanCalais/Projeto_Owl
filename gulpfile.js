0d4ab8eebe2a99c3ab90d3ddff06bfc7f3869aba
LuanCalais <luancalais@gmail.com>
2022-05-01 14:46:42 -0300

    tarefa que minifica js e watch no default

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,28 @@ import imagemin from 'gulp-imagemin';
 // Css Minify
 const css_from = 'assets/css/style.css';
 const css_to = 'assets/css/';
-gulp.task('minify-css', ()=>{
+gulp.task('minify-css', () => {
     return gulp.src(css_from)
-    .pipe(cssMin().on('error', (err)=>{
-        console.log(`Ocorreu um erro no minify-css: ${err}`)
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(css_to));
+        .pipe(cssMin().on('error', (err) => {
+            console.log(`Ocorreu um erro no minify-css: ${err}`)
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(css_to));
+});
+
+// Js Minify
+const js_from = 'assets/js/main.js';
+const js_to = 'assets/js/';
+gulp.task('minify-js', () => {
+    return gulp.src(js_from)
+        .pipe(jsMin().on('error', (err)=>{
+            console.log(`Ocorreu um erro no minify-js: ${err}`)
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(js_to));
 });
 
 // Watch
-gulp.task('default', ()=>{
-    gulp.watch(['assets/css/*'], gulp.series('minify-css'));
+gulp.task('default', () => {
+    gulp.watch(['assets/css/*', 'assets/js/*'], gulp.series('minify-css', 'minify-js'));
 })
